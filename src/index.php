fd8ede7602183e279ae8322ac011785571c59c9d
PedritoSola85200 <efraincnb@gmail.com>
2025-10-20 13:26:24 -0600

    Se agrego la funcion para crear un usuario

--- src/index.php
+++ src/index.php
@@ -2,3 +2,8 @@
 
 require "init_db.php";
 
+function crearUsuario($pdo, $nombre, $correo, $contrasena) {
+    $sql = "INSERT INTO usuarios (nombre, correo, contrasena) VALUES (?, ?, ?)";
+    $stmt = $pdo->prepare($sql);
+    return $stmt->execute([$nombre, $correo, $contrasena]); 
+}
