c741be3427c92468bd182f7005aa5be57fa0263b
PedritoSola85200 <efraincnb@gmail.com>
2025-10-20 13:27:00 -0600

    Se agrego la funcion para traer a todos los  usuarios

--- src/index.php
+++ src/index.php
@@ -7,3 +7,10 @@ function crearUsuario($pdo, $nombre, $correo, $contrasena) {
     $stmt = $pdo->prepare($sql);
     return $stmt->execute([$nombre, $correo, $contrasena]); 
 }
+
+function leerUsuarios($pdo) {
+    $sql = "SELECT id, nombre, correo, fecha_creacion FROM usuarios ORDER BY id DESC";
+    $stmt = $pdo->query($sql);
+    $usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);  
+    return $usuarios; 
+}
